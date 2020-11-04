14f87eae76956c9e5f27222c1ed36540caa609aa
zoydbird <zoydbird@protonmail.com>
2020-11-04 16:53:07 +0000

    adds image command, and boilerplate for boards command

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ USER_ID = os.getenv('DISCORD_USER_ID')
 bot = commands.Bot(command_prefix='!')
 
 NSFW_BOARDS = [
-'h', 'd', 'gif'
+'h', 'd', 'gif', 'r9k'
 ]
 
 
@@ -29,20 +29,20 @@ async def on_ready():
     async for server in servers:
         print(f'connected to {server.name}\n')
  
-@bot.command(name='shitpost', help='Generates shitposts from a 4chan board: Example use: "!shitpost /b/')
+@bot.command(name='shitpost', help='Generates shitposts from a 4chan board: "!shitpost /b/"')
 async def shitpost(ctx, board):
 
     cleansed_board = board.replace( '/', '' )
 
     if ctx.channel.is_nsfw() is False:
         if cleansed_board in NSFW_BOARDS:
-            await ctx.send('No lewds in this channel!!!')
+            await ctx.send('Shitposts from nsfw boards can only be posted in nsfw channels, sorry!')
             return
 
 
     
     if shitposter.board_loaded(cleansed_board) is False:
-        message = f'No data loaded for {board}, please use !load{board}'
+        message = f'No data loaded for {board}, please use !load {board}'
     else:
         print(f'generating shitpost from {board}')
         mc, images = shitposter.load_or_train_board(cleansed_board)
@@ -53,7 +53,7 @@ async def shitpost(ctx, board):
     await ctx.send(message)
 
 
-@bot.command(name='load')
+@bot.command(name='load', help='Loads a board, only Zoyd can use this command.')
 async def load(ctx, board):
 
     cleansed_board = board.replace( '/', '' )
@@ -62,10 +62,35 @@ async def load(ctx, board):
         await ctx.send(f'Loading posts for {board}, bare with a few minutes...')
         mc, images = shitposter.load_or_train_board(cleansed_board)
         await ctx.send(f'{board} loaded')
+    else:
+        await ctx.send(f'Only Zoyd can load boards, ask him if you want a particular board :~)')
     
+@bot.command(name='image', help='Posts a random image from a 4chan board: "!img /c/"')
+async def image(ctx, board):
+    cleansed_board = board.replace( '/', '' )
 
-# @bot.command(name='boards', help='Lists the loaded boards!')
-# async def boards(ctx):
+    if ctx.channel.is_nsfw() is False:
+        if cleansed_board in NSFW_BOARDS:
+            await ctx.send('No lewds in this channel!!!')
+            return
+
+    if shitposter.board_loaded(cleansed_board) is False:
+        message = f'No data loaded for {board}, please use !load {board}'
+    else:
+        print(f'generating shitpost from {board}')
+        mc, images = shitposter.load_or_train_board(cleansed_board)
+        image = shitposter.grab_image(images, cleansed_board)
+        message = f'{image}'
+
+    await ctx.send(message)
+
+
+
+@bot.command(name='boards', help='Lists the loaded boards!')
+async def boards(ctx):
+    loaded_boards = shitposter.loaded_boards()
+
+    ctx.send(f'Command not implemented yet, sorry!')
 
 
 bot.run(TOKEN)
